b197f247743c90a9bad2d6809f1f2cc8476b6ebe
Hasna Hena Mow <hasnahenamow@gmail.com>
2022-05-21 07:05:31 +0600

    Manage Inventory Done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import About from './Pages/Home/About/About';
 import ViewItem from './Pages/ViewItem/ViewItem';
 import ManageItems from './Pages/ManageItems/ManageItems';
 import UserItems from './Pages/UserItems/UserItems';
+import AddItem from './Pages/AddItem/AddItem';
+import { ModalsProvider } from '@mantine/modals';
 
 function App() {
   const [colorScheme, setColorScheme] = useState('light');
@@ -30,45 +32,55 @@ function App() {
         withGlobalStyles
         withNormalizeCSS
       >
-        <NotificationsProvider>
-          <div className="App">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route
-                path="view/:itemId"
-                element={
-                  <RequireAuth>
-                    <ViewItem />
-                  </RequireAuth>
-                }
-              ></Route>
-              <Route
-                path="manage"
-                element={
-                  <RequireAuth>
-                    <ManageItems />
-                  </RequireAuth>
-                }
-              ></Route>
-              <Route
-                path="user/items"
-                element={
-                  <RequireAuth>
-                    <UserItems />
-                  </RequireAuth>
-                }
-              ></Route>
-            </Routes>
-            {/* <div style={{ flexGrow: '1' }}></div> (Use a blank div with flex-grow:1 to fill unused spaced right before the footer) */}
-            {/* <!-- Any content below this will always be at bottom. --> */}
-            <Footer />
-          </div>
-        </NotificationsProvider>
+        <ModalsProvider>
+          <NotificationsProvider>
+            <div className="App">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route
+                  path="manage"
+                  element={
+                    <RequireAuth>
+                      <ManageItems />
+                    </RequireAuth>
+                  }
+                ></Route>
+                <Route
+                  path="user/items"
+                  element={
+                    <RequireAuth>
+                      <UserItems />
+                    </RequireAuth>
+                  }
+                ></Route>
+                <Route
+                  path="view/:itemId"
+                  element={
+                    <RequireAuth>
+                      <ViewItem />
+                    </RequireAuth>
+                  }
+                ></Route>
+                <Route
+                  path="addItem/"
+                  element={
+                    <RequireAuth>
+                      <AddItem />
+                    </RequireAuth>
+                  }
+                ></Route>
+              </Routes>
+              {/* <div style={{ flexGrow: '1' }}></div> (Use a blank div with flex-grow:1 to fill unused spaced right before the footer) */}
+              {/* <!-- Any content below this will always be at bottom. --> */}
+              <Footer />
+            </div>
+          </NotificationsProvider>
+        </ModalsProvider>
       </MantineProvider>
     </ColorSchemeProvider>
   );
--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -1,7 +1,136 @@
-import React from 'react';
+import {
+  Button,
+  Grid,
+  Image,
+  Table,
+  Center,
+  Loader,
+  ScrollArea,
+  MediaQuery,
+  Dialog,
+  Group,
+  Text,
+} from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import useItems from '../../hooks/useItems';
+import { ItemCard } from '../../Components/ItemCard/ItemCard';
+import { useEffect, useState } from 'react';
+import { useModals } from '@mantine/modals';
 
 const ManageItems = () => {
-  return <div>ManageItems</div>;
+  const [items, setItems] = useItems();
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+  const [opened, setOpened] = useState(false);
+  const modals = useModals();
+
+  useEffect(() => {
+    let timer = setTimeout(() => setLoading(false), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const handleDeleteItem = (id) => {
+    modals.openConfirmModal({
+      title: 'Please confirm your action',
+      children: (
+        <Text size="sm">
+          This action is so important that you are required to confirm it with a
+          modal. Please click one of these buttons to proceed.
+        </Text>
+      ),
+      labels: { confirm: 'Confirm', cancel: 'Cancel' },
+      onCancel: () => console.log('Cancel'),
+      onConfirm: () => deleteConfirm(),
+    });
+
+    const deleteConfirm = () => {
+      fetch(`http://localhost:5000/delete/${id}`, {
+        method: 'DELETE',
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setItems(items.filter((item) => item._id !== id));
+        });
+    };
+  };
+
+  if (loading) {
+    return (
+      <Center>
+        <Loader color="dark" size="xl" variant="dots" />
+      </Center>
+    );
+  }
+  return (
+    <div className=" flex flex-col items-center justify-center mx-0 px-0 md:mx-20 md:px-20">
+      <h1 className="my-5 text-xl md:text-2xl">
+        Manage All Items from One Place
+      </h1>
+      <Button
+        variant="gradient"
+        gradient={{ from: 'blue', to: 'teal' }}
+        style={{ marginBottom: 50, width: 300, margin: 30 }}
+        onClick={() => navigate(`/addItem`)}
+      >
+        Add New Item
+      </Button>
+
+      <MediaQuery
+        query="(max-width: 767px) and (min-width: 300px)"
+        styles={{ width: 350 }}
+      >
+        <ScrollArea>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Image</th>
+                <th>View Item</th>
+                <th>Delet Item</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item._id}>
+                  <td>{item.name}</td>
+                  <td>
+                    {'$ '}
+                    {item.price}
+                  </td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    <img className="max-w-[30px] h-auto" src={item.image} />
+                  </td>
+                  <td>
+                    <Button
+                      variant="outline"
+                      onClick={() => navigate(`/view/${item._id}`)}
+                    >
+                      View
+                    </Button>
+                  </td>
+                  <td>
+                    <Button
+                      color="red"
+                      onClick={() => handleDeleteItem(item._id)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </ScrollArea>
+      </MediaQuery>
+    </div>
+  );
 };
 
 export default ManageItems;
